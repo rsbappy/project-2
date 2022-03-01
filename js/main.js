6058b22074600399191985d694e2342b7d778fa8
bappy <bappykhan226@gmail.com>
2022-03-01 22:03:00 +0600

    update

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+const swiper = new Swiper('.mySwiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+      autoplay:{
+	delay:2500,
+    },
+    // Navigation arrows
+
+  
+    // And if we need scrollbar
+   
+  });
+
+
