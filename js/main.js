740f9b335dadc1cb5801da3648550b9d0ab8ddc3
bappy <bappykhan226@gmail.com>
2022-03-06 20:27:02 +0600

    update

--- js/main.js
+++ js/main.js
@@ -40,3 +40,26 @@ const swiper = new Swiper('.mySwiper', {
       navCollapse.classList.remove("show");
     })
   })
+
+  document.addEventListener("DOMContentLoaded", ()=>{
+    function counter(id ,start , end, duration) {
+      let obj =document.getElementById(id),
+      current= start,
+      range = end - start, 
+      increment = end > start ? 1: -1,
+      step = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(()=> {
+        current += increment;
+        obj.textContent = current;
+        if (current ==end) {
+            clearInterval(timer);
+        }
+      },step);
+    }
+      counter("count1", 0, 1287, 2000);
+      counter("count2", 100, 500, 2000);
+      counter("count3", 0, 800, 2000);
+      counter("count4", 0, 1000, 2000);
+
+
+  })
